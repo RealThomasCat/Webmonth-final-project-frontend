898cf908853aa69d34643bcb2c077cbbbdf45566
Yashveer Singh <yashveer.rtc@gmail.com>
2022-08-13 21:55:16 +0530

    CreateNote executed

--- pages/createNotes/index.js
+++ pages/createNotes/index.js
@@ -1,11 +1,37 @@
-const createNoteInput = document.querySelector('.create-note-input');
+const apiUrl = "https://secure-basin-07537.herokuapp.com";
 
-createNoteInput.addEventListener("input", (e) => {
-    console.log(e.target.value);
+const createNoteButton = document.querySelector(".create-note-button");
+
+const token = localStorage.getItem("jwt");
+
+const body = document.querySelector("body");
+
+window.addEventListener("load", () => {
+  body.classList.add("visible");
 });
 
-const body = document.querySelector('body');
+createNoteButton.addEventListener("click", () => {
+  const content = document.querySelector(".create-note-input").value;
+  const heading = document.querySelector(".create-note-heading").value;
 
-window.addEventListener('load', () => {
-    body.classList.add('visible');
-})
+  if (token) {
+    fetch(`${apiUrl}/note/add`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        authorization: token,
+      },
+      body: JSON.stringify({ content, heading }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.message) {
+            location.href = "/pages/dashboard/dashboard.html";
+        }
+      })
+      .catch((err) => {
+        alert("Error creating note");
+        console.log(err);
+      });
+  }
+});
